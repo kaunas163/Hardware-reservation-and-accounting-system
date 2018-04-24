023bd3cc95a4860fd18f00b1c59c79c42a2fb283
Ieva <ieva.jaksaityte@gmail.com>
2018-04-25 02:11:03 +0300

    Add Gulp for compiling SASS files.

--- HardwareReservationAndAccountingSystem/App_Start/BundleConfig.cs
+++ HardwareReservationAndAccountingSystem/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace HardwareReservationAndAccountingSystem
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/main.css"));
         }
     }
 }
---
+++ HardwareReservationAndAccountingSystem/Content/Gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function () {
+    gulp.src('sass/main.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['styles'], function () {
+    gulp.watch('sass/**/*.scss', ['styles']);
+});
