93dcdceae602b1bce7615bfe868f1e6c1f4e6246
Ieva <ieva.jaksaityte@gmail.com>
2018-04-25 10:09:18 +0300

    Add new equipment bundles.

--- HardwareReservationAndAccountingSystem/Controllers/EquipmentBundlesController.cs
+++ HardwareReservationAndAccountingSystem/Controllers/EquipmentBundlesController.cs
@@ -3,14 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HardwareReservationAndAccountingSystem.Models;
 
 namespace HardwareReservationAndAccountingSystem.Controllers
 {
     public class EquipmentBundlesController : Controller
     {
+        private ApplicationDbContext _context = new ApplicationDbContext();
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var bundles = _context.EquipmentBundles.OrderBy(x => x.Title).ToList();
+            return View(bundles);
+        }
+
+        [HttpPost]
+        public ActionResult Save(EquipmentBundle equipmentBundle)
+        {
+            if (equipmentBundle.Id == 0)
+            {
+                _context.EquipmentBundles.Add(equipmentBundle);
+            }
+            else
+            {
+                var equipmentInDb = _context.EquipmentBundles.Single(x => x.Id == equipmentBundle.Id);
+                equipmentInDb.Title = equipmentBundle.Title;
+                equipmentInDb.Description = equipmentBundle.Description;
+            }
+
+            _context.SaveChanges();
+            return RedirectToAction("Index", "EquipmentBundles");
         }
     }
 }
