97b1e62e165c2368e20040fa798869040c457f2e
Ieva <ieva.jaksaityte@gmail.com>
2018-04-25 03:52:34 +0300

    Left sidebar overflow fix.

---
+++ HardwareReservationAndAccountingSystem/Scripts/Scripts.js
@@ -0,0 +1,8 @@
+﻿/***************************************************
+ * Sidebar links, expand on click
+***************************************************/
+
+//$(".left-sidebar .navbar-links .first-level > li > a").on("click", function (e) {
+//    e.preventDefault();
+//    $(this).next(".second-level").slideDown();
+//});
