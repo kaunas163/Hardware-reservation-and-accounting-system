db59c8b28947d06f600fdf5dc5b39c60d79fa60c
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-04-22 17:03:50 +0300

    update gulp, move files around

--- HardwareReservationAndAccountingSystem/App_Start/BundleConfig.cs
+++ HardwareReservationAndAccountingSystem/App_Start/BundleConfig.cs
@@ -8,32 +8,36 @@ namespace HardwareReservationAndAccountingSystem
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
-                        "~/Scripts/jquery-ui.min.js"));
+//            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+//                        "~/Scripts/jquery-{version}.js",
+//                        "~/Scripts/jquery-ui.min.js"
+//                        ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+//            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+//                        "~/Scripts/jquery.validate*"
+//                        ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+//            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+//                        "~/Scripts/modernizr-*"
+//                        ));
 
-            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
-//                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js",
-                      "~/Scripts/moment.min.js",
-                      "~/Scripts/fullcalendar.min.js",
-                      "~/Scripts/fullcalendar-lt-locale.js",
-                      "~/Scripts/lightbox.min.js",
-                      "~/Scripts/scripts.js"));
+//            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+////                      "~/Scripts/bootstrap.js",
+//                      "~/Scripts/respond.js",
+//                      "~/Scripts/moment.min.js",
+//                      "~/Scripts/fullcalendar.min.js",
+//                      "~/Scripts/fullcalendar-lt-locale.js",
+//                      "~/Scripts/lightbox.min.js",
+//                      "~/Scripts/scripts.js"
+//                      ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/fullcalendar.min.css",
-                      "~/Content/lightbox.min.css",
-                      "~/Content/main.css"));
+//            bundles.Add(new StyleBundle("~/Content/css").Include(
+//                      "~/Content/bootstrap.css",
+//                      "~/Content/fullcalendar.min.css",
+//                      "~/Content/lightbox.min.css",
+//                      "~/Content/main.css"));
         }
     }
 }
