4e6e5afdcc5f586ddacdf68c77885351daf4ea49
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2019-04-23 22:50:20 +0300

    content view modes enum

---
+++ HardwareReservationAndAccountingSystem/Data/ContentViewMode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HardwareReservationAndAccountingSystem.Data
+{
+    public enum ContentViewMode
+    {
+        List,
+        Table,
+        Calendar,
+        Grid
+    }
+}
